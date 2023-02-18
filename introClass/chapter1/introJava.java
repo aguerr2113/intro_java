fe0bb7814f19fc83c59b75a47a0ad894e00bc093
aguerrero2113 <aguerrero2113@github.com>
2023-02-17 20:38:29 -0600

    started an intro to java class

---
+++ introClass/chapter1/introJava.java
@@ -0,0 +1,13 @@
+package introClass.chapter1;
+
+/**
+ * introJava
+ */
+public class introJava {
+
+    public static void main(String[] args) {
+        System.out.println("Name: Artemio Guerrero");
+        System.out.println("Major:Computer Science");
+        System.out.println("Movie:the Fellowship of the Ring");
+    }
+}
---
+++ introClass/chapter2/lab2.java
@@ -0,0 +1,23 @@
+package introClass.chapter2;
+
+public class lab2 {
+    
+    public static void main(String[] args) {
+        int og_recipe;
+        int tomato_sauce;
+        int garlic;
+        int tomato_paste;
+        int oregano;
+
+
+        og_recipe = 4;
+        tomato_sauce = (2/og_recipe);
+        garlic= (2/og_recipe);
+        tomato_paste = ((1/3)/og_recipe);
+        oregano = (1/og_recipe);
+
+        System.out.println("The value of og is:" + og_recipe  + "The value of tom p is:" + tomato_paste + "The value of tom s is:" + tomato_sauce + "The value of gar is:" + garlic + "The value of oregano is:" + oregano);
+
+    }
+
+}
